29256a80b8cf2686f95680205e74ccaf0ed2f637
JohanFernandoLema <johan.13fernando@gmail.com>
2023-05-25 11:36:40 -0400

    first project added successfully to the projects landpage

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,5 +1,6 @@
 import './index.scss'
 import image from '../../assets/images/camping.png'
+import Loader from 'react-loaders'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
@@ -7,22 +8,26 @@ const Projects = () => {
   return (
     <div className="wrapper">
       <div className="firstProject">
-        <img src={image} alt="camping" className="project" />
-
-        <h1> Camping Website</h1>
-        <p className="paragraph">
-          E-commerce application built using React, React components, along with
-          Sass for UI enhancement. Website main purpose is for delivering a
-          creative and interactive user experience regardless of the complex
-          content the application has.
-        </p>
-        <Link
-          to={'https://github.com/JohanFernandoLema'}
-          className="linkGithub"
-        >
-          <FontAwesomeIcon icon={faGithub} className="icon" />
-        </Link>
+        <div className="campingImage">
+          <img src={image} alt="camping" className="project" />
+        </div>
+        <div className="cardContent">
+          <h1 className="mainHeading"> Camping Website</h1>
+          <p className="paragraph">
+            E-commerce application built using React, React components, along
+            with Sass for UI enhancement. Website main purpose is for delivering
+            a creative and interactive user experience regardless of the complex
+            content the application has.
+          </p>
+          <Link
+            to={'https://github.com/JohanFernandoLema'}
+            className="linkGithub"
+          >
+            <FontAwesomeIcon icon={faGithub} className="icon" />
+          </Link>
+        </div>
       </div>
+      <Loader type="pacman" />
     </div>
   )
 }
